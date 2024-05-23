d809e1daabd39dacadc00e4e53c2bf8262177bfe
Grant Smith <57376089+ggsmith842@users.noreply.github.com>
2024-05-22 19:16:30 -0600

    Update hexlib.ml
    Added doc string for each function and updated encoder to use Cstruct like in decoder. Formatted example printing.

--- data/cookbook/encode-decode-hex/hexlib.ml
+++ data/cookbook/encode-decode-hex/hexlib.ml
@@ -3,30 +3,36 @@ packages: [Hex, Cstruct]
 discussion: |
   - **Using the Hex and Cstruct libraries to decode a hexidecimal string to an ascii string.
   - **Using the Hex library to convert an ascii string to a hexidecimal encoded message.
-  - **Leverages the Hex and Cstruct libraries.
+  - **Uses the Hex and Cstruct libraries.
 ---
 
-(* Decode a hex string  *)
-let decode_hex_string (hex_string : string) : string =
+
+(* [decode_hex_string hex_string] is the string represented by [hex_string].
+    Raises: Invalid_argument if [hex_string] is not valid hex. *)
+let decode_hex_string hex_string =
   let byte_string = Hex.to_cstruct (`Hex hex_string) in
   let decoded_string = Cstruct.to_string byte_string in
   decoded_string
 ;;
 
-(* Encode a string to hex *)
-let encode_to_hex (message : string) : string =
-  Hex.of_string message
+(** [encode_to_hex message] is the hex string representation of [message]. *)
+let encode_to_hex message =
+  let byte_string = Cstruct.of_string message in
+  let hex_string = Hex.of_cstruct byte_string in
+  Hex.to_string hex_string
 ;;
 
-(* Example *)
-let secret_message = "48656c6c6f2c20576f726c6421"
+(* Example usage *)
+let secret_message = "48656c6c6f2c20576f726c6421";;
+Printf.printf "Secret message: %s\n" secret_message
+
 let decrypted_message = decode_hex_string secret_message;;
 
 (* Show the decrypted message *)
-print_endline decrypted_message
+Printf.printf "Decoded message: %s\n" decrypted_message
 
 (* Encrypt the message back to hexidecimal *)
 let encoded_message = encode_to_hex secret_message;;
 
 (* Show the hexidecimal encoded message *)
-Hex.show encoded_message
+Printf.printf "Encoded message: %s\n" encoded_message;;
